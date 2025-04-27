afc108a0916feae2499bec5dc24b8a71a907f775
yxm <yxm.dev@gmx.ie>
2025-04-27 15:14:28 -0300

    ...

--- sphinx_json_index/main.py
+++ sphinx_json_index/main.py
@@ -4,6 +4,8 @@ import os
 
 class JsonIndexBuilder(Builder):
     name = 'jsonindex'
+    format = 'json'
+
     def init(self):
         self.output = []
 
@@ -15,12 +17,12 @@ class JsonIndexBuilder(Builder):
             doctree = self.env.get_doctree(docname)
             title = self.env.titles[docname].astext() if docname in self.env.titles else ""
             text = doctree.astext()
-            href = self.config.html_context['pathto'](docname + self.config.html_file_suffix or '.html', 1)
+            href = self.app.builder.get_relative_uri(self.config.master_doc, docname)
             self.output.append({
                 'id': docname,
                 'title': title,
                 'content': text,
-                'href': href
+                'href': href,
             })
         outfname = os.path.join(self.outdir, 'searchindex.json')
         with open(outfname, 'w', encoding='utf-8') as f:
